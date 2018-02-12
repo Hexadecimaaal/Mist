cd1909292e4a251a73eb9c545f0d341211e8583f
Hexadecimaaal <hexadecimaaal@gmail.com>
2018-02-12 21:29:36 +0800

    add binder and vektor support

--- src/io/github/hexadecimaaal/mist/parser.kt
+++ src/io/github/hexadecimaaal/mist/parser.kt
@@ -1,14 +1,20 @@
 package io.github.hexadecimaaal.mist
 
 sealed class Token
-private object LBrace : Token()
-private object RBrace : Token()
+private object LParen : Token()
+private object RParen : Token()
+private object LBracket : Token()
+private object RBracket : Token()
 private object End : Token()
+private object As : Token()
+private data class TOperator(val name : String) : Token()
 private data class TIdentifier(val name : String) : Token()
 
 sealed class AST
 data class Identifier(val name : String) : AST()
 data class SExp(val head : AST, val tail : List<AST>) : AST()
+data class Vect(val content : List<AST>) : AST()
+data class Bind(val name : AST, val type : AST) : AST()
 
 class Parser {
     private var pos : Int = 0
@@ -25,31 +31,80 @@ class Parser {
     private fun parseAST() : AST {
         val x = getToken()
         return when (x) {
-            is LBrace -> parseList()
+            is LParen -> parseSExp()
             is TIdentifier -> Identifier(x.name)
-            else -> throw Error()
+            is RParen -> TODO()
+            is End -> TODO()
+            is As -> TODO()
+            is TOperator -> Identifier(x.name)
+            is LBracket -> parseVect()
+            is RBracket -> TODO()
         }
     }
 
-    private fun parseList() : SExp {
+    private fun parseSExp() : SExp {
         val head = parseAST()
         var x = peekToken()
         val l = ArrayList<AST>()
-        while (x !is RBrace) {
+        while (x !is RParen) {
             l.add(parseAST())
             x = peekToken()
         }
-        getToken() // => RBrace
+        getToken() // => RParen
         return SExp(head, l)
     }
 
-    private tailrec fun getToken() : Token = when(input[pos]) {
-        '(' -> { pos++; LBrace }
-        ')' -> { pos++; RBrace }
+    private fun parseVect() : Vect {
+        var x = peekToken()
+        val l = ArrayList<AST>()
+        while (x !is RBracket) {
+            val name = parseAST()
+            x = peekToken()
+            if (x is As) {
+                getToken() // => As
+                l.add(Bind(name, parseAST()))
+                x = peekToken()
+            }
+            else {
+                l.add(name)
+            }
+        }
+        getToken() // => RBracket
+        return Vect(l)
+    }
+
+    private tailrec fun getToken() : Token = when (input[pos]) {
+        '(' -> {
+            pos++
+            LParen
+        }
+        ')' -> {
+            pos++
+            RParen
+        }
+        '[' -> {
+            pos++
+            LBracket
+        }
+        ']' -> {
+            pos++
+            RBracket
+        }
         '\u0000' -> End
         in AZaz -> eatTIdentifier()
-        in Space -> { pos++; getToken() }
-        else -> throw Error("qaq")
+        in Space -> {
+            pos++
+            getToken()
+        }
+        ':' -> {
+            pos++
+            when (input[pos]) {
+                in Symbols -> TOperator(":" + eatTOperator().name)
+                else -> As
+            }
+        }
+        in Symbols -> eatTOperator()
+        else -> TODO()
     }
 
     private fun peekToken() : Token {
@@ -68,10 +123,19 @@ class Parser {
         return TIdentifier(s)
     }
 
+    private fun eatTOperator() : TOperator {
+        var s = ""
+        while (input[pos] in Symbols) {
+            s += input[pos]
+            pos++
+        }
+        return TOperator(s)
+    }
 
     companion object {
         const val AZaz = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
-        val AZaz09 = AZaz + "1234567890"
+        val AZaz09 = AZaz + "1234567890-"
         val Space = " \n\t\r"
+        val Symbols = "!#$%&@`;+*:,<.>/?_=^~\\¥|-"
     }
 }
--- test/io/github/hexadecimaaal/mist/test/parser-test.kt
+++ test/io/github/hexadecimaaal/mist/test/parser-test.kt
@@ -5,22 +5,55 @@ import io.github.hexadecimaaal.mist.*
 import org.junit.jupiter.api.Assertions.assertEquals
 
 class TestParser {
-    @Test fun test1() {
+    @Test fun testIdentifier() {
         val p = Parser()
         assertEquals(p.parse("QAQ\u0000"),
                 listOf(Identifier("QAQ")))
     }
-    @Test fun test2() {
+    @Test fun testList() {
         val p = Parser()
         assertEquals(p.parse("QAQ qwq QuQ \u0000"),
                 listOf(Identifier("QAQ"), Identifier("qwq"), Identifier("QuQ")))
     }
-    @Test fun test3() {
+    @Test fun testSExp() {
         val p = Parser()
         assertEquals(p.parse("(S (S (S O))) \u0000"),
                 listOf(SExp(Identifier("S"),
                         listOf(SExp(Identifier("S"),
                                 listOf(SExp(Identifier("S"),
                                         listOf(Identifier("O")))))))))
+    }
+    @Test fun testSExpTwice() {
+        val p = Parser()
+        assertEquals(p.parse("(S (S (S O))) (S (S O))\u0000"),
+                listOf(SExp(Identifier("S"),
+                        listOf(SExp(Identifier("S"),
+                                listOf(SExp(Identifier("S"),
+                                        listOf(Identifier("O"))))))),
+                        SExp(Identifier("S"),
+                                listOf(SExp(Identifier("S"),
+                                        listOf(Identifier("O")))))))
+    }
+    @Test fun testVect() {
+        val p = Parser()
+        p.parse("""
+            (is [qwq]
+                (->
+                    (forall [P] (-> (not (not P)) P))
+                    (forall [Q] (or (not Q) Q))))
+                    """.trimIndent() + '\u0000')
+    }
+    @Test fun testBinding() {
+        val p = Parser()
+        p.parse("""
+            (is [++]
+                (forall [X : Type]
+                    (-> (list X) (list X) (list X))))
+
+            (def [++ l1 l2]
+                (case l1
+                    [ [_ : nil] l2
+                      [[h t] : cons] (++ t (cons h l2))] ))
+            """.trimIndent() + '\u0000')
     }
 }
